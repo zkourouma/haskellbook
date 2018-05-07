55cf9447ed6ebbf206af6d07cb379b850c2597f1
Zack Kourouma <zack@secondspectrum.com>
2018-05-07 09:13:51 -0700

    Fix caesar cipher with less elegant approach

--- src/Cipher.hs
+++ src/Cipher.hs
@@ -9,11 +9,16 @@ caesar :: Int -> String -> String
 caesar offset = map shifty
   where
     shifty c =
-      let f = mod (offset + ord c)
+      let shifted = (offset + ord c)
       in chr $
          if isUpper c
-           then ord 'A' + mod (f (ord 'Z')) (ord 'A') + 1
-           else ord 'a' + mod (f (ord 'z')) (ord 'a') + 1
+           then caesar' (ord 'A') (ord 'Z') shifted
+           else caesar' (ord 'a') (ord 'z') shifted
+
+caesar' lower upper val
+  | val > upper = lower + mod val (1 + upper)
+  | val < lower = val - lower + upper + 1
+  | otherwise = val
 
 unCaesar :: Int -> String -> String
-unCaesar offset = caesar (-offset)
+unCaesar offset = caesar $ negate offset
