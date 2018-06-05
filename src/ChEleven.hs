cfe49f45c2a616940d1a68d3f39fb44356ec3e25
Zack Kourouma <zack@secondspectrum.com>
2018-06-05 09:43:58 -0700

    List comprehension to create all the programmers using record syntax

--- src/ChEleven.hs
+++ src/ChEleven.hs
@@ -67,3 +67,36 @@ instance TooMany (Int, String) where
 
 instance TooMany (Int, Int) where
   tooMany (n, m) = n + m > 42
+
+data OperatingSystem
+  = GnuPlusLinux
+  | OpenBSDPlusNevermindJustBSDStill
+  | Mac
+  | Windows
+  deriving (Eq, Show)
+
+data ProgLang
+  = Haskell
+  | Agda
+  | Idris
+  | PureScript
+  deriving (Eq, Show)
+
+data Programmer = Programmer
+  { os   :: OperatingSystem
+  , lang :: ProgLang
+  } deriving (Eq, Show)
+
+allOperatingSystems :: [OperatingSystem]
+allOperatingSystems =
+  [GnuPlusLinux, OpenBSDPlusNevermindJustBSDStill, Mac, Windows]
+
+allLanguages :: [ProgLang]
+allLanguages = [Haskell, Agda, Idris, PureScript]
+
+allProgrammers :: [Programmer]
+allProgrammers =
+  [ Programmer {os = os, lang = lang}
+  | os <- allOperatingSystems
+  , lang <- allLanguages
+  ]
