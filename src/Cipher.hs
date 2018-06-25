9b4e3c4e80399ff2505bdf32fc4eef4701178d8e
Zack Kourouma <zack@kourouma.me>
2018-06-25 09:34:12 -0700

    Fix indentation and add spec for new cipher

--- src/Cipher.hs
+++ src/Cipher.hs
@@ -10,10 +10,10 @@ caesar offset = map shifty
   where
     shifty c =
       let shifted = (offset + ord c)
-      in chr $
-         if isUpper c
-           then caesar' (ord 'A') (ord 'Z') shifted
-           else caesar' (ord 'a') (ord 'z') shifted
+       in chr $
+          if isUpper c
+            then caesar' (ord 'A') (ord 'Z') shifted
+            else caesar' (ord 'a') (ord 'z') shifted
 
 caesar' lower upper val
   | val > upper = lower + mod val (1 + upper)
@@ -22,3 +22,8 @@ caesar' lower upper val
 
 unCaesar :: Int -> String -> String
 unCaesar offset = caesar $ negate offset
+
+-- takes an input key and repeats it as long as the cipher text. the ord of each of
+-- the chars from the input key becomes the shift for its corresponding cipher text char
+vigenere :: String -> String -> String
+vigenere = undefined
