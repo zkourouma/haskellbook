23c4a3544ffd6fa55c06cb865537ab9bae5fc1a0
Zack Kourouma <zack@kourouma.me>
2018-08-15 09:53:06 -0700

    Add chapter twelve
    only up through the unfolds section

---
+++ src/ChTwelve.hs
@@ -0,0 +1,161 @@
+module ChTwelve where
+
+-- String Processing
+replaceThe :: String -> String
+replaceThe sentence =
+  let w:ws = words sentence
+      a = fromMaybe ("a" :: String) (notThe w)
+   in case ws of
+        [] -> a
+        _  -> a ++ " " ++ replaceThe (unwords ws)
+
+notThe :: String -> Maybe String
+notThe "the" = Nothing
+notThe s     = Just s
+
+countTheBeforeVowel :: String -> Integer
+countTheBeforeVowel sentence =
+  let w:ws = words sentence
+      c = theBeforeVowel w ws
+   in case ws of
+        [] -> c
+        _  -> c + countTheBeforeVowel (unwords ws)
+
+theBeforeVowel :: String -> [String] -> Integer
+theBeforeVowel _ [] = 0
+theBeforeVowel "the" (l:_) =
+  if isVowel (head l)
+    then 1
+    else 0
+theBeforeVowel _ _ = 0
+
+countVowels :: String -> Integer
+countVowels = foldl countVowel 0
+
+countVowel :: Integer -> Char -> Integer
+countVowel i c =
+  if isVowel c
+    then i + 1
+    else i
+
+isVowel :: Char -> Bool
+isVowel c = c `elem` "AEIOUYaeiouy"
+
+-- Validate the word
+newtype Word' =
+  Word' String
+  deriving (Eq, Show)
+
+vowels = "aeiou"
+
+mkWord :: String -> Maybe Word'
+mkWord s =
+  let (consts, vols) = foldl mkWord' (0, 0) s
+   in if vols > consts
+        then Nothing
+        else Just (Word' s)
+
+mkWord' :: (Integer, Integer) -> Char -> (Integer, Integer)
+mkWord' (cs, vs) c =
+  if c `elem` vowels
+    then (cs, vs + 1)
+    else (cs + 1, vs)
+
+-- It's only Natural
+data Nat
+  = Zero
+  | Succ Nat
+  deriving (Eq, Show)
+
+natToInteger :: Nat -> Integer
+natToInteger Zero       = 0
+natToInteger (Succ nat) = 1 + natToInteger nat
+
+integerToNat :: Integer -> Maybe Nat
+integerToNat i
+  | i < 0 = Nothing
+  | i == 0 = Just Zero
+  | otherwise =
+    case integerToNat (i - 1) of
+      Nothing  -> Nothing
+      Just nat -> Just (Succ nat)
+
+-- Small library for Maybe
+isJust :: Maybe a -> Bool
+isJust Nothing = False
+isJust _       = True
+
+isNothing :: Maybe a -> Bool
+isNothing = not . isJust
+
+mayybee :: b -> (a -> b) -> Maybe a -> b
+mayybee b _ Nothing  = b
+mayybee _ f (Just a) = f a
+
+fromMaybe :: a -> Maybe a -> a
+fromMaybe a Nothing  = a
+fromMaybe _ (Just a) = a
+
+listToMaybe :: [a] -> Maybe a
+listToMaybe []     = Nothing
+listToMaybe (a:as) = Just a
+
+maybeToList :: Maybe a -> [a]
+maybeToList Nothing  = []
+maybeToList (Just a) = [a]
+
+catMaybes :: [Maybe a] -> [a]
+catMaybes = foldr catMaybes' []
+
+catMaybes' :: Maybe a -> [a] -> [a]
+catMaybes' Nothing as  = as
+catMaybes' (Just a) as = a : as
+
+flipMaybe :: [Maybe a] -> Maybe [a]
+flipMaybe = foldr flipMaybe' (Just [])
+
+flipMaybe' :: Maybe a -> Maybe [a] -> Maybe [a]
+flipMaybe' Nothing _          = Nothing
+flipMaybe' _ Nothing          = Nothing
+flipMaybe' (Just a) (Just as) = Just (a : as)
+
+-- Small library for Either
+lefts' :: [Either a b] -> [a]
+lefts' = foldr (\(Left a) as -> a : as) []
+
+rights' :: [Either a b] -> [b]
+rights' = foldr (\(Right b) bs -> b : bs) []
+
+partitionEithers' :: [Either a b] -> ([a], [b])
+partitionEithers' es = (as, bs)
+  where
+    as = lefts' es
+    bs = rights' es
+
+eitherMaybe' :: (b -> c) -> Either a b -> Maybe c
+eitherMaybe' f (Right b) = Just (f b)
+eitherMaybe' _ _         = Nothing
+
+either' :: (a -> c) -> (b -> c) -> Either a b -> c
+either' f _ (Left a)  = f a
+either' _ g (Right b) = g b
+
+eitherMaybe'' :: (b -> c) -> Either a b -> Maybe c
+eitherMaybe'' f = either' (const Nothing) (Just . f)
+
+-- Unfolds
+myIterate :: (a -> a) -> a -> [a]
+myIterate f a = go [a]
+  where
+    go (x:xs) = x : go (f x : x : xs)
+
+myUnfoldr :: (b -> Maybe (a, b)) -> b -> [a]
+myUnfoldr f = go
+  where
+    go n =
+      case f n of
+        Nothing      -> []
+        Just (a, b') -> a : go b'
+
+betterIterate :: (a -> a) -> a -> [a]
+betterIterate f = myUnfoldr (\b -> Just (b, f b))
---
+++ test/ChTwelveSpec.hs
@@ -0,0 +1,37 @@
+module ChTwelveSpec
+  ( main
+  , spec
+  ) where
+
+import           Test.Hspec
+import           Test.QuickCheck
+
+import           ChTwelve
+
+main :: IO ()
+main = hspec spec
+
+spec :: Spec
+spec = do
+  describe "replaceThe" $
+    it "swaps 'the' with 'a'" $
+    replaceThe "the cow loves us" `shouldBe` "a cow loves us"
+  describe "countTheBeforeVowel" $
+    it "counts the 'the's before vowels" $ do
+      countTheBeforeVowel "the cow" `shouldBe` 0
+      countTheBeforeVowel "the evil cow" `shouldBe` 1
+  describe "countVowels" $
+    it "counts the vowels" $ do
+      countVowels "the cow" `shouldBe` 2
+      countVowels "Mikolajczak" `shouldBe` 4
+  describe "natToInteger" $ do
+    it "knows Zero" $ natToInteger Zero `shouldBe` 0
+    it "knows Succ" $ do
+      natToInteger (Succ Zero) `shouldBe` 1
+      natToInteger (Succ (Succ Zero)) `shouldBe` 2
+  describe "integerToNat" $ do
+    it "knows Zero" $ integerToNat 0 `shouldBe` Just Zero
+    it "knows Succ" $ do
+      integerToNat 1 `shouldBe` Just (Succ Zero)
+      integerToNat 2 `shouldBe` Just (Succ (Succ Zero))
+    it "throws out negative numbers" $ integerToNat (-1) `shouldBe` Nothing
