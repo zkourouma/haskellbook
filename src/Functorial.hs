90c7cb9b2d77d9b8fbe79b3238a12454f68d8ef1
Zack Kourouma <zack@kourouma.me>
2018-09-24 09:04:29 -0700

    Add functor instances for chapter 16 exercises

--- src/Functorial.hs
+++ src/Functorial.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE FlexibleInstances #-}
+
 module Functorial where
 
 import           Test.QuickCheck
@@ -162,3 +164,92 @@ instance (Arbitrary a, Arbitrary b) => Arbitrary (Four' a b) where
 type Four'Id = Four' Int Int -> Bool
 
 type FourPrimeComp = Four' Int Int -> IntToInt -> IntToInt -> Bool
+
+data Quant a b
+  = Finance
+  | Desk a
+  | Bloor b
+
+instance Functor (Quant a) where
+  fmap _ Finance   = Finance
+  fmap _ (Desk a)  = Desk a
+  fmap f (Bloor b) = Bloor (f b)
+
+newtype K a b =
+  K a
+
+instance Functor (K a) where
+  fmap _ (K a) = K a
+
+newtype Flip f a b =
+  Flip (f b a)
+  deriving (Eq, Show)
+
+instance Functor (Flip K a) where
+  fmap f (Flip (K a)) = Flip (K (f a))
+
+newtype EvilGoateeConst a b =
+  GoatyConst b
+
+instance Functor (EvilGoateeConst a) where
+  fmap f (GoatyConst b) = GoatyConst (f b)
+
+newtype LiftItOut f a =
+  LiftItOut (f a)
+
+instance Functor f => Functor (LiftItOut f) where
+  fmap f (LiftItOut g) = LiftItOut (fmap f g)
+
+data Parappa f g a =
+  DaWrappa (f a)
+           (g a)
+
+instance (Functor f, Functor g) => Functor (Parappa f g) where
+  fmap f (DaWrappa f' g) = DaWrappa (fmap f f') (fmap f g)
+
+data IgnoreOne f g a b =
+  IgnoreSomething (f a)
+                  (g b)
+
+instance Functor g => Functor (IgnoreOne f g a) where
+  fmap f (IgnoreSomething f' g) = IgnoreSomething f' (fmap f g)
+
+data Notorious g o a t =
+  Notorious (g o)
+            (g a)
+            (g t)
+
+instance Functor g => Functor (Notorious g o a) where
+  fmap f (Notorious o a t) = Notorious o a (fmap f t)
+
+data List a
+  = Nil
+  | Cons a
+         (List a)
+
+instance Functor List where
+  fmap _ Nil         = Nil
+  fmap f (Cons a as) = Cons (f a) (fmap f as)
+
+data GoatLord a
+  = NoGoat
+  | OneGoat a
+  | MoreGoats (GoatLord a)
+              (GoatLord a)
+              (GoatLord a)
+
+instance Functor GoatLord where
+  fmap _ NoGoat            = NoGoat
+  fmap f (OneGoat a)       = OneGoat (f a)
+  fmap f (MoreGoats a b c) = MoreGoats (fmap f a) (fmap f b) (fmap f c)
+
+data TalkToMe a
+  = Halt
+  | Print String
+          a
+  | Read (String -> a)
+
+instance Functor TalkToMe where
+  fmap _ Halt        = Halt
+  fmap f (Print s a) = Print s (f a)
+  fmap f (Read g)    = Read (f . g)
