64327c9d4d5f81cc729df9cfc1b40cf64d0d22d1
Zack Kourouma <zack@kourouma.me>
2018-10-03 09:33:54 -0700

    Identity and Constant instances of Applicative

--- src/Applicatives.hs
+++ src/Applicatives.hs
@@ -39,3 +39,25 @@ y2 = lookup 2 $ zip xs2 ys2
 
 summed :: Maybe Integer
 summed = pure sum <*> ((,) <$> x2 <*> y2)
+
+newtype Identity a =
+  Identity a
+  deriving (Eq, Ord, Show)
+
+instance Functor Identity where
+  fmap f (Identity a) = Identity (f a)
+
+instance Applicative Identity where
+  pure = Identity
+  (<*>) (Identity f) (Identity a) = Identity (f a)
+
+newtype Constant a b = Constant
+  { getConstant :: a
+  } deriving (Eq, Ord, Show)
+
+instance Functor (Constant a) where
+  fmap _ (Constant a) = Constant a
+
+instance Monoid a => Applicative (Constant a) where
+  pure a = Constant mempty
+  (<*>) (Constant f) (Constant _) = Constant f
