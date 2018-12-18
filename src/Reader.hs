5f8b05754d8a52448365bac092f7647e1b61ba0e
Zack Kourouma <zack@kourouma.me>
2018-12-18 09:18:36 -0800

    Start Reader chapter with myLiftA2 and Applicative

---
+++ src/Reader.hs
@@ -0,0 +1,48 @@
+{-# LANGUAGE InstanceSigs #-}
+
+module Reader where
+
+import           Data.Char
+
+cap :: String -> String
+cap = map toUpper
+
+rev :: String -> String
+rev = reverse
+
+composed :: String -> String
+composed = cap . rev
+
+fmapped :: String -> String
+fmapped = cap <$> rev
+
+tupled :: String -> (String, String)
+tupled = (,) <$> cap <*> rev
+
+tupled0 :: String -> (String, String)
+tupled0 = do
+  a <- cap
+  b <- rev
+  return (a, b)
+
+newtype Reader r a = Reader
+  { runReader :: r -> a
+  }
+
+ask :: Reader a a
+ask = Reader id
+
+myLiftA2 :: Applicative f => (a -> b -> c) -> f a -> f b -> f c
+myLiftA2 f a b = f <$> a <*> b
+
+asks :: (r -> a) -> Reader r a
+asks = Reader
+
+instance Functor (Reader r) where
+  fmap f (Reader ra) = Reader (f . ra)
+
+instance Applicative (Reader r) where
+  pure :: a -> Reader r a
+  pure a = Reader $ const a
+  (<*>) :: Reader r (a -> b) -> Reader r a -> Reader r b
+  (Reader rab) <*> (Reader ra) = Reader $ \r -> rab r (ra r)
