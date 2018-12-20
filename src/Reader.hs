0bb218d934b115da9dc5951aca551f472eb4f418
Zack Kourouma <zack@kourouma.me>
2018-12-20 09:17:59 -0800

    Implement monad instance for Reader

--- src/Reader.hs
+++ src/Reader.hs
@@ -46,3 +46,8 @@ instance Applicative (Reader r) where
   pure a = Reader $ const a
   (<*>) :: Reader r (a -> b) -> Reader r a -> Reader r b
   (Reader rab) <*> (Reader ra) = Reader $ \r -> rab r (ra r)
+
+instance Monad (Reader r) where
+  return = pure
+  (>>=) :: Reader r a -> (a -> Reader r b) -> Reader r b
+  (Reader ra) >>= aRb = Reader $ \r -> runReader (aRb . ra $ r) r
